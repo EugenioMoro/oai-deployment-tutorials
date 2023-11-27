dd76d85258c6d97b24a160acec51a777e906b1cc
moari <eugenio.moro@polimi.it>
2023-11-27 23:45:22 +0100

    util scripts added

---
+++ scripts/run_gnb.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd cmake_targets/ran_build/build
+sudo ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/gnb.sa.band78.fr1.106PRB.usrpb210.conf --gNBs.[0].min_rxtxtime 6 --rfsim --sa
